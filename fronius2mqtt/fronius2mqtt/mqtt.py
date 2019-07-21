65a1323dc84f056fc85a281855b2bbdd7f54144e
Gerrit Beine <mail@gerritbeine.de>
2019-07-21 15:06:14 +0200

    Private variables start with _

--- fronius2mqtt/fronius2mqtt/mqtt.py
+++ fronius2mqtt/fronius2mqtt/mqtt.py
@@ -5,18 +5,18 @@ import paho.mqtt.client as mqtt
 class Mqtt:
 
 	def __init__(self, config):
-		self.config = config
+		self._config = config
 
 	def connect(self):
-		self.client = mqtt.Client()
-		self.client.username_pw_set(self.config['user'], self.config['password'])
-		self.client.connect(self.config['host'], self.config['port'])
-		self.client.loop_start()
+		self._client = mqtt.Client()
+		self._client.username_pw_set(self._config['user'], self._config['password'])
+		self._client.connect(self._config['host'], self._config['port'])
+		self._client.loop_start()
 
 	def disconnect(self):
 		self.client.disconnect()
 
 	def publish(self, topic, payload):
-		topic = "{}/{}".format(self.config['topic'], topic)
+		topic = "{}/{}".format(self._config['topic'], topic)
 		logging.info("Publish %s: %s, %s, %s", topic, payload, self._config["qos"], self._config["retain"])
 		self._client.publish(topic, payload, self._config["qos"], self._config["retain"])
